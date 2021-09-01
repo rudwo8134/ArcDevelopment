a66a5e403139be1df811e776580cec7aa930cb93
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-09-01 12:43:39 -0400

    add the typography color grey to white

--- src/components/ui/Theme.jsx
+++ src/components/ui/Theme.jsx
@@ -1,10 +1,10 @@
-import { createTheme } from "@material-ui/core/styles"
+import { createMuiTheme } from '@material-ui/core/styles';
 
-const arcBlue="#0b72b9"
-const arcOrange = "#ffba60"
-const arcGrey = "#868686"
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
-export default createTheme({
+export default createMuiTheme({
   palette: {
     common: {
       blue: arcBlue,
@@ -21,15 +21,9 @@ export default createTheme({
     tab: {
       fontFamily: 'Raleway',
       textTransform: 'none',
-      fontWeight: '700',
+      fontWeight: 700,
+      color: 'white',
       fontSize: '1rem',
-    },
-    h2: {
-      fontFamily: 'Raleway',
-      fontSize: '2.5rem',
-      fontWeight: '700',
-      color: arcBlue,
-      lineHeight: 1.5,
     },
     estimate: {
       fontFamily: 'Pacifico',
@@ -37,51 +31,57 @@ export default createTheme({
       textTransform: 'none',
       color: 'white',
     },
-    body1: {
-      fontSize: '1.25rem',
-      fontWeight: '300',
-      color: arcGrey,
-    },
-    h4: {
+    h2: {
       fontFamily: 'Raleway',
-      fontSize: '1.75rem',
-      fontWeight: '700',
+      fontWeight: 700,
+      fontSize: '2.5rem',
       color: arcBlue,
-    },
-    h6: {
-      fontFamily: 'Raleway',
-      color: arcBlue,
-      fontWeight: '500',
-      lineHeight: 1
+      lineHeight: 1.5,
     },
     h3: {
       fontFamily: 'Pacifico',
       fontSize: '2.5rem',
       color: arcBlue,
     },
+    h4: {
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 500,
+      fontFamily: 'Raleway',
+      color: arcBlue,
+    },
     subtitle1: {
       fontSize: '1.25rem',
-      fontWeight: '300',
+      fontWeight: 300,
       color: arcGrey,
     },
     subtitle2: {
-      fontSize: '1.25rem',
-      fontWeight: '300',
       color: 'white',
+      fontWeight: 300,
+      fontSize: '1.25rem',
     },
-    learnbutton: {
-      borderColor: arcBlue,
-      color: arcBlue,
-      borderWidth: 2,
-      textTransform: 'none',
-      borderRadius: 50,
-      fontFamily: 'Roboto',
-      fontWeight: 'bold',
+    body1: {
+      fontSize: '1.25rem',
+      color: arcGrey,
+      fontWeight: 300,
     },
     caption: {
       fontSize: '1rem',
       fontWeight: 300,
       color: arcGrey,
+    },
+    learnbutton: {
+      borderColor: arcBlue,
+      borderWidth: 2,
+      textTransform: 'none',
+      color: arcBlue,
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
     },
   },
   overrides: {
